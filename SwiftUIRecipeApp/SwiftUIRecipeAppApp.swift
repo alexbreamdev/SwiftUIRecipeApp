97b0decc640917d08261afa2b65db8cedba0a29a
alexbreamdev <kortes39dev@gmail.com>
2022-09-23 23:42:45 +0300

    Initial Commit

---
+++ SwiftUIRecipeApp/SwiftUIRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRecipeAppApp.swift
+//  SwiftUIRecipeApp
+//
+//  Created by Oleksii Leshchenko on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
